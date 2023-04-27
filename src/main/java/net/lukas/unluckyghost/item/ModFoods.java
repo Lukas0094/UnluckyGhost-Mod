6c6b85f00e0f2ee171b22ba105c8c52f21d22c2f
Lukas0094 <124440453+Lukas0094@users.noreply.github.com>
2023-04-27 19:53:02 +0200

    Food Properties changed

--- src/main/java/net/lukas/unluckyghost/item/ModFoods.java
+++ src/main/java/net/lukas/unluckyghost/item/ModFoods.java
@@ -5,5 +5,5 @@ import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.food.FoodProperties;
 
 public class ModFoods {
-    public static final FoodProperties BLUE_POTATO = (new FoodProperties.Builder()).fast().nutrition(2).saturationMod(0.7F).effect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 200, 2), 1F).build();
+    public static final FoodProperties BLUE_POTATO = (new FoodProperties.Builder()).fast().nutrition(1).saturationMod(0.5F).effect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 200, 2), 1F).build();
 }
